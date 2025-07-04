7effe0be00d709696a9549aa0dbc1f6b8a06524f
ultrasbox <yamaki3970@gmail.com>
2025-07-04 10:04:09 +0900

    Update main.php

--- main.php
+++ main.php
@@ -10,6 +10,14 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     } else {
         // ここでデータベースに保存する処理を行う
         // データベース接続処理など
+        // ユーザーから受け取ったパスワード
+$raw_password = 'user_input_password';
+
+// パスワードをハッシュ化する
+$hashed_password = password_hash($raw_password, PASSWORD_DEFAULT);
+
+// データベースにハッシュ化されたパスワードを保存
+echo 'ハッシュ化されたパスワード: ' . $hashed_password;
         echo "ログイン情報を受け取りました。";
     }
 }

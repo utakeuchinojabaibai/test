a5a90f3e86d63dcc7cb3f614decb29fad3a2e922
ultrasbox <yamaki3970@gmail.com>
2025-07-07 08:32:01 +0900

    Update and rename config.sample.js to config.js

--- tutorial2/js/config.sample.js
+++
@@ -1,8 +0,0 @@
-var NebulaConfig = {
-    "tenant":  "",
-    "appId":   "",
-    "appKey":  "",
-    "baseUri": "https://baas.example.com/api/",
-    "offline" : false,
-    "debugMode": "debug"
-};

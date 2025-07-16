3bdd688125987c443b1fd468028090ee20e813f8
utakeuchinojabaibai <183874927+utakeuchinojabaibai@users.noreply.github.com>
2025-07-17 08:53:41 +0900

    Delete tutorial2 directory

--- tutorial2/js/config.js
+++
@@ -1,8 +0,0 @@
-var NebulaConfig = {
-    "tenant": "5657fe61240d3e15d40acd37",
-    "appId": "56ea65bb240d3e27263af479",
-    "appKey": "KPQOlhm949cN6s1cXNsSRFwh8qUTKh8KE81SCFC7",
-    "baseUri": "https://baas.example.com/api/",
-    "offline" : false,
-    "debugMode": "debug"
-};
